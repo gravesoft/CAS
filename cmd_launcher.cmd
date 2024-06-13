bc4b01df0f84b4a84c5297bd513a8522cc359520
abbodi1406 <33669284+abbodi1406@users.noreply.github.com>
2024-06-14 00:02:52 +0300

    Detect PS existence

--- cmd_launcher.cmd
+++ cmd_launcher.cmd
@@ -1,4 +1,10 @@
 @echo off
+for %%# in (powershell.exe) do @if "%%~$PATH:#"=="" (
+echo.
+echo Windows PowerShell is not installed
+pause
+exit /b
+)
 set "_PSf=%~dp0Check-Activation-Status.ps1"
 setlocal EnableDelayedExpansion
 set "_PSf=!_PSf:'=''!"
